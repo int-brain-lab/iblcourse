d6788c2625407642c2540e5e862ee92d223aeb90
Gaelle <gaelle.chapuis.ibl@gmail.com>
2023-07-18 11:49:15 +0200

    load

--- sources/behavior_course/scripts/data_loading.py
+++ sources/behavior_course/scripts/data_loading.py
@@ -29,8 +29,8 @@ def get_trial_sess_subj_df(eid, one):
     df_sess = pd.DataFrame.from_dict(sess)
     # Rename column id to eid
     df_sess = df_sess.rename(columns={"id": "eid"})
-    # Get date
-    df_sess['date'] = pd.to_datetime(df_sess['start_time']).dt.date
+    # Get date and time
+    df_sess['date_time'] = pd.to_datetime(df_sess['start_time'])  # access date with .dt.date
     # Remove some columns
     df_sess = df_sess.drop(columns=['projects', 'url', 'number', 'start_time'])
 
@@ -65,13 +65,13 @@ for index, eid in enumerate(sessions_bw):
 df_subj_all = df_subj_all.drop_duplicates(subset=["subj_id"], keep='first')
 
 # Sort
-df_sess_all = df_sess_all.sort_values(by=['subject', 'date'])
+df_sess_all = df_sess_all.sort_values(by=['subject', 'date_time'])
 df_subj_all = df_subj_all.sort_values(by=['subject'])
 
 # Save dataframes
-df_sess_all.to_csv(LOCAL_PATH.joinpath('sessions.csv'))
-df_subj_all.to_csv(LOCAL_PATH.joinpath('subjects.csv'))
-df_trial_all.to_csv(LOCAL_PATH.joinpath('trials.csv'))
+df_sess_all.to_parquet(LOCAL_PATH.joinpath('sessions.pqt'), index=False)  # index=False to get rid of Unnamed column
+df_subj_all.to_parquet(LOCAL_PATH.joinpath('subjects.pqt'), index=False)
+df_trial_all.to_parquet(LOCAL_PATH.joinpath('trials.pqt'), index=False)
 
 ##
 #
