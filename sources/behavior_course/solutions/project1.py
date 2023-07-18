ddeae968a6a33a120aa2e8726bbfbc8d0bab9de2
Gaelle <gaelle.chapuis.ibl@gmail.com>
2023-07-18 15:05:00 +0200

    F vs m

--- sources/behavior_course/solutions/project1.py
+++ sources/behavior_course/solutions/project1.py
@@ -12,3 +12,13 @@ df_trial = pd.read_parquet(LOCAL_PATH.joinpath('trials.pqt'))
 ##
 # Compute reaction time
 df_trial['reaction_time'] = df_trial['response_times'] - df_trial['goCue_times']
+
+# Merge
+df_sess = df_sess.merge(df_subj[['subject', 'sex']], on='subject')
+df_trial = df_trial.merge(df_sess[['eid', 'sex']], on='eid')
+
+# Count how many trials for M / F mice
+tr_fm = df_trial.groupby('sex')['reaction_time'].count()
+print(tr_fm)
+# Proportion of Female over Male mice
+pr_fm = tr_fm['F'] / tr_fm['M']
