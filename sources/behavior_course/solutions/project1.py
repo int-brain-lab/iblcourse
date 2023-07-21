8bd203c6e83fbfee13558fe756ec3ca0831b620b
Gaelle <gaelle.chapuis.ibl@gmail.com>
2023-07-21 14:15:30 +0200

    neg rt

--- sources/behavior_course/solutions/project1.py
+++ sources/behavior_course/solutions/project1.py
@@ -15,10 +15,20 @@ df_trial['reaction_time'] = df_trial['response_times'] - df_trial['goCue_times']
 
 # Merge
 df_sess = df_sess.merge(df_subj[['subject', 'sex']], on='subject')
-df_trial = df_trial.merge(df_sess[['eid', 'sex']], on='eid')
+df_trial = df_trial.merge(df_sess[['eid', 'sex', 'subject']], on='eid')  # Added in subject for boxplot
 
 # Count how many trials for M / F mice
 tr_fm = df_trial.groupby('sex')['reaction_time'].count()
 print(tr_fm)
 # Proportion of Female over Male mice
 pr_fm = tr_fm['F'] / tr_fm['M']
+
+# Get negative RTs
+df_trial_neg_rt = df_trial[df_trial['reaction_time'] < 0]
+
+# Plot the distribution of trial number for negative reaction time per subject
+df_trial_neg_rt.plot.box(column="trial_number", by="subject")
+# Count how many trials for each subject
+tr_sub = df_trial_neg_rt.groupby('subject')['reaction_time'].count()
+print(tr_sub)
+# outlier subject: KS023
