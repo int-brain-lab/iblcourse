eea4f04dbd11d9eb635cc084e84b1398568c5af2
Gaelle <gaelle.chapuis.ibl@gmail.com>
2023-07-26 12:56:50 +0200

    edits

--- sources/behavior_course/solutions/project1.py
+++ sources/behavior_course/solutions/project1.py
@@ -1,6 +1,7 @@
 ##
 import pandas as pd
 from pathlib import Path
+import matplotlib.pyplot as plt
 
 LOCAL_PATH = Path('/Users/gaelle/Documents/Work/Course')
 
@@ -11,11 +12,16 @@ df_trial = pd.read_parquet(LOCAL_PATH.joinpath('trials.pqt'))
 
 ##
 # Compute reaction time
+df_trial['reaction_time_trig'] = df_trial['response_times'] - df_trial['goCueTrigger_times']
 df_trial['reaction_time'] = df_trial['response_times'] - df_trial['goCue_times']
+df_trial['reaction_time_diff'] = df_trial['reaction_time'] - df_trial['reaction_time_trig']
 
 # Merge
 df_sess = df_sess.merge(df_subj[['subject', 'sex']], on='subject')
 df_trial = df_trial.merge(df_sess[['eid', 'sex', 'subject']], on='eid')  # Added in subject for boxplot
+
+##
+# Questions in text
 
 # Count how many trials for M / F mice
 tr_fm = df_trial.groupby('sex')['reaction_time'].count()
@@ -32,3 +38,20 @@ df_trial_neg_rt.plot.box(column="trial_number", by="subject")
 tr_sub = df_trial_neg_rt.groupby('subject')['reaction_time'].count()
 print(tr_sub)
 # outlier subject: KS023
+
+##
+# Further questions
+
+# Get positive RTs
+df_trial_pos_rt = df_trial[df_trial['reaction_time'] > 0]
+
+# Plot the distribution of reaction time for male and female mice using `hist`, and `boxplot`
+# Histogram
+df_trial_pos_rt.groupby('sex')['reaction_time'].plot(kind='kde')
+# Add legends for the curves (sex)
+plt.legend(df_trial_pos_rt.groupby('sex').sex.dtype.index.to_list(), title='Sex')
+# Box plot
+df_trial_pos_rt.plot.box(column="reaction_time", by="sex")
+
+# Plot the distribution of reaction time per subject using `boxplot`
+df_trial_pos_rt.plot.box(column="reaction_time", by="subject")
